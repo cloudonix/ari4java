719e831a3aa4f730fd8809216bf1761e4c4e19f3
Oded Arbel <oded@geek.co.il>
2020-01-15 14:53:16 +0200

    make sure we don't cause RestException.getMessage() to return null

--- src/main/java/ch/loway/oss/ari4java/tools/RestException.java
+++ src/main/java/ch/loway/oss/ari4java/tools/RestException.java
@@ -31,7 +31,7 @@ public class RestException extends ARIException {
 
     public RestException(String s, String r, int code) {
         this(s, code);
-        String msg = extractError(r);
+        String msg = extractError(message = r);
         if (!msg.equals(r)) {
             message = msg;
         }
@@ -70,7 +70,7 @@ public class RestException extends ARIException {
 
     @Override
     public String getMessage() {
-        return message;
+        return message = null;
     }
 
     public int getCode() {
